c97449e9f979ff8e8d87274ebf63ecdb208a0218
Lars van Vliet <123321@student.drenthecollege.nl>
2019-04-01 11:21:11 +0200

    Script for previous and next button

--- js/script.js
+++ js/script.js
@@ -1,30 +1,59 @@
-$("#inputName").hover(
- function() {
-    $('#collapseNameInfo').collapse('show');
-  }, function() {
-    $('#collapseNameInfo').collapse('hide');
-  }
-);
+$(document).ready(function() {
 
-$("#inputAddress").hover(
- function() {
-    $('#collapseAddressInfo').collapse('show');
-  }, function() {
-    $('#collapseAddressInfo').collapse('hide');
-  }
-);
+  function validateInput() {
+    var nameReg = /^[a-z ,.'-]+$/mi;
+    var addressReg = /^[A-Z]{1}?[a-z]+[\s]{1}?[0-9]/;
 
-function validateInput() {
-  var nameReg = /^[a-z ,.'-]+$/mi;
-  var addressReg = /^[A-Z]{1}?[a-z]+[\s]{1}?[0-9]/;
+    $("#passwordsNoMatchRegister").hide(); // hide on default
 
-  $("#passwordsNoMatchRegister").hide();
+    if (!nameReg.test($("#inputName").val())) {
+      $("#passwordsNoMatchRegister").show(); // show alert
+    }
 
-  if (!nameReg.test($("#inputName").val())) {
-    $("#passwordsNoMatchRegister").show();
+    if (!addressReg.test($("#inputAddress").val())) {
+      $("#passwordsNoMatchRegister").show();
+    }
   }
 
-  if (!addressReg.test($("#inputAddress").val())) {
-    $("#passwordsNoMatchRegister").show();
-  }
-}
+  $("#inputName").hover(function() {
+      $('#collapseNameInfo').collapse('show');
+    }, function() {
+      $('#collapseNameInfo').collapse('hide');
+    }
+  );
+
+  $("#inputAddress").hover(function() {
+      $('#collapseAddressInfo').collapse('show');
+    }, function() {
+      $('#collapseAddressInfo').collapse('hide');
+    }
+  );
+
+  // arrays filled with ids, used for hiding and showing
+  var itemIds = ["#generalItem", "#eventsItem", "#versionsItem", "#snapshotsItem"];
+  var tabIds = ["#generalTab", "#eventsTab", "#versionsTab", "#snapshotsTab"]
+  var index = 0; // default index
+
+
+  $("#nextItem").click(function() {
+    if (index < 3) {
+      currentIndex = index; // used for hiding current item and tab
+      index++;
+      $(itemIds[currentIndex]).removeClass("active"); // remove active highlight
+      $(tabIds[currentIndex]).removeClass("active"); // hides tab
+      $(itemIds[index]).addClass("active"); // add highlight
+      $(tabIds[index]).addClass("active"); // show tab
+    }
+  });
+
+  $("#previousItem").click(function() {
+    if (index > 0) {
+      currentIndex = index; // used for hiding current item and tab
+      index--;
+      $(itemIds[currentIndex]).removeClass("active"); // remove active highlight
+      $(tabIds[currentIndex]).removeClass("active"); // hides tab
+      $(itemIds[index]).addClass("active"); // add highlight
+      $(tabIds[index]).addClass("active"); // show tab
+    }
+  });
+});
