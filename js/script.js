6a676b6b269510a9defbc3b6cf7d66533c5947cc
Lars van Vliet <123321@student.drenthecollege.nl>
2019-04-02 10:40:57 +0200

    added comments

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  // function for filtering the table on home page
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
     $("#myTable tr").filter(function() {
@@ -7,9 +8,10 @@ $(document).ready(function() {
     });
   });
 
-  function validateInput() {
-    var nameReg = /^[a-z ,.'-]+$/mi;
-    var addressReg = /^[A-Z]{1}?[a-z]+[\s]{1}?[0-9]/;
+  // check the input from the form
+  $("#submitForm").click(function() {
+    var nameReg = /^[a-z ,.'-]+$/mi; // regex for name
+    var addressReg = /^[A-Z]{1}?[a-z]+[\s]{1}?[0-9]/; // regex for address
 
     $("#passwordsNoMatchRegister").hide(); // hide on default
 
@@ -20,8 +22,9 @@ $(document).ready(function() {
     if (!addressReg.test($("#inputAddress").val())) {
       $("#passwordsNoMatchRegister").show();
     }
-  }
+  });
 
+  // show information about name when hovering over the input of name
   $("#inputName").hover(function() {
       $('#collapseNameInfo').collapse('show');
     }, function() {
@@ -29,6 +32,7 @@ $(document).ready(function() {
     }
   );
 
+  // show information about address when hovering over the input of address
   $("#inputAddress").hover(function() {
       $('#collapseAddressInfo').collapse('show');
     }, function() {
@@ -41,7 +45,8 @@ $(document).ready(function() {
   var tabIds = ["#generalTab", "#eventsTab", "#versionsTab", "#snapshotsTab"]
   var index = 0; // default index
 
-  $(".list-group-item").click(function() { // function used for setting index to clicked item
+  // function used for setting index to clicked item
+  $(".list-group-item").click(function() {
     $(".tab-pane").removeClass("active"); // remove active from tabs, otherwise there will be multiple categories shown
     switch ($(this).attr('id')) {
       case "generalItem":
@@ -65,8 +70,20 @@ $(document).ready(function() {
         break;
     }
   });
+  
+  // function for handling click on previous
+  $("#previousItem").click(function() {
+    if (index > 0) {
+      currentIndex = index; // used for hiding current item and tab
+      index--;
+      $(itemIds[currentIndex]).removeClass("active"); // remove active highlight
+      $(tabIds[currentIndex]).removeClass("active"); // hides tab
+      $(itemIds[index]).addClass("active"); // add highlight
+      $(tabIds[index]).addClass("active"); // show tab
+    }
+  });
 
-
+  // function for handling click on next
   $("#nextItem").click(function() {
     if (index < 3) {
       currentIndex = index; // used for hiding current item and tab
@@ -78,14 +95,4 @@ $(document).ready(function() {
     }
   });
 
-  $("#previousItem").click(function() {
-    if (index > 0) {
-      currentIndex = index; // used for hiding current item and tab
-      index--;
-      $(itemIds[currentIndex]).removeClass("active"); // remove active highlight
-      $(tabIds[currentIndex]).removeClass("active"); // hides tab
-      $(itemIds[index]).addClass("active"); // add highlight
-      $(tabIds[index]).addClass("active"); // show tab
-    }
-  });
 });
